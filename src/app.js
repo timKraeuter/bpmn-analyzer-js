8a2354a377821a4d3e3df4a19803aae43f233dac
Tim Kräuter <tim.kraeuterr@gmail.com>
2023-11-12 21:03:41 +0100

    first test using bundling example

---
+++ src/app.js
@@ -0,0 +1,25 @@
+// we use stringify to inline an example XML document
+import pizzaDiagram from "../resources/initial.bpmn";
+
+// make sure you added bpmn-js to your your project
+// dependencies via npm install --save bpmn-js
+import BpmnViewer from "bpmn-js";
+
+var viewer = new BpmnViewer({
+  container: "#canvas",
+});
+
+viewer
+  .importXML(pizzaDiagram)
+  .then(function (result) {
+    const { warnings } = result;
+
+    console.log("success !", warnings);
+
+    viewer.get("canvas").zoom("fit-viewport");
+  })
+  .catch(function (err) {
+    const { warnings, message } = err;
+
+    console.log("something went wrong:", warnings, message);
+  });
