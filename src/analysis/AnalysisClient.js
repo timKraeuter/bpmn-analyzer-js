a0abccf2cc64cf1213562f23726173ffef29830e
Tim Kräuter <tim.kraeuterr@gmail.com>
2023-11-13 11:21:37 +0100

    Requesting check bpmn

--- src/analysis/AnalysisClient.js
+++ src/analysis/AnalysisClient.js
@@ -1,8 +1,26 @@
 const websocket_url = "ws://localhost:8071/debug";
 export default function AnalysisClient(eventBus) {
   eventBus.on("analysis.required", (diagramXML) => {
-    console.log("Analysis should be run for:");
-    console.log(diagramXML.xml);
+    console.log("Analysis should be run!");
+
+    fetch("http://localhost:3001/check_bpmn", {
+      method: "POST",
+      body: JSON.stringify({
+        bpmn_file_content: diagramXML.xml,
+        properties_to_be_checked: [
+          "Safeness",
+          "OptionToComplete",
+          "ProperCompletion",
+          "NoDeadActivities"
+        ]
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+
   });
 }
 
