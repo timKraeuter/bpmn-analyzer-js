8d76ab0c9a259a4c28691af3dc81ec10be1cf770
Tim Kräuter <tim.kraeuterr@gmail.com>
2023-11-17 09:34:57 +0100

    fix

--- src/app.js
+++ src/app.js
@@ -152,7 +152,7 @@ function setEncoded(link, name, data) {
     );
     link.setAttribute("download", name);
   } else {
-    link.classList.removeClass("active");
+    link.classList.remove("active");
   }
 }
 
