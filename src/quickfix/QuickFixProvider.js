1ed6996bdda99b0488dedee52982ec6bd274ad5d
Tim Kräuter <tim.kraeuterr@gmail.com>
2023-12-17 10:31:25 +0100

    Replace one element programmatically

--- src/quickfix/QuickFixProvider.js
+++ src/quickfix/QuickFixProvider.js
@@ -1,5 +1,17 @@
-export default function QuickFixProvider(eventBus) {
-  console.log("quickfix injected");
+export default function QuickFixProvider(
+  bpmnReplace,
+  elementRegistry,
+  eventBus,
+) {
+  const pgType = "bpmn:ParallelGateway";
+  eventBus.on("analysis.done", () => {
+    const gateway = elementRegistry.get("Gateway_0g0wslj");
+    const targetElement = {
+      type: pgType,
+    };
+    if (gateway.type !== pgType) {
+      bpmnReplace.replaceElement(gateway, targetElement);
+    }
+  });
 }
-
-QuickFixProvider.$inject = ["eventBus"];
+QuickFixProvider.$inject = ["bpmnReplace", "elementRegistry", "eventBus"];
