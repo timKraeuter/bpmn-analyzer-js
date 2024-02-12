43dca945382cfea4208840ebc4d47170e427c297
Tim Kräuter <tim.kraeuterr@gmail.com>
2024-02-12 14:51:22 +0100

    First token animation

--- src/counter-example-visualization/CounterExampleVisualizer.js
+++ src/counter-example-visualization/CounterExampleVisualizer.js
@@ -1,35 +1,48 @@
-export default function CounterExampleVisualizer(animation, eventBus) {
-  this._animation = animation;
-  this._eventBus = eventBus;
-
-  this._eventBus.on("analysis.done", (result) => {
+export default function CounterExampleVisualizer(
+  animation,
+  eventBus,
+  elementRegistry,
+) {
+  eventBus.on("analysis.done", (result) => {
     result.property_results.forEach((propertyResult) => {
       addClickListenerIfNotFulfilled(propertyResult);
     });
   });
-}
 
-CounterExampleVisualizer.$inject = ["animation", "eventBus"];
+  function addClickListenerIfNotFulfilled(propertyResult) {
+    const property = document.getElementById(propertyResult.property);
+    if (!property.fulfilled && propertyResult.counter_example) {
+      property.classList.add("clickable");
+      property.addEventListener("click", function () {
+        visualizeCounterExample(propertyResult);
+      });
+    } else {
+      property.classList.remove("clickable");
+    }
+  }
 
-function addClickListenerIfNotFulfilled(propertyResult) {
-  const property = document.getElementById(propertyResult.property);
-  if (!property.fulfilled && propertyResult.counter_example) {
-    property.classList.add("clickable");
-    property.addEventListener("click", function () {
-      visualizeCounterExample(propertyResult);
+  /**
+   * Visualize the counter example on click.
+   * @param {PropertyResult} propertyResult
+   */
+  function visualizeCounterExample(propertyResult) {
+    if (propertyResult.counter_example.start_state.snapshots.length !== 1) {
+      console.error("Start state has more than one snapshot");
+      return;
+    }
+    const snapshot = propertyResult.counter_example.start_state.snapshots[0];
+    Object.entries(snapshot.tokens).forEach(([key, value]) => {
+      const element = elementRegistry.get(key);
+      const scope = { element };
+      animation.animate(element, scope, () => {
+        console.log("Animation done");
+      });
     });
-  } else {
-    property.classList.remove("clickable");
+
+    // propertyResult.counter_example.transitions.forEach((transition) => {
+    //   console.log("Animate each transition", transition);
+    // });
   }
 }
 
-/**
- * Visualize the counter example on click.
- * @param {PropertyResult} propertyResult
- */
-function visualizeCounterExample(propertyResult) {
-  console.log("Start animation", propertyResult.counter_example.start_state);
-  propertyResult.counter_example.transitions.forEach((transition) => {
-    console.log("Animate each transition", transition);
-  });
-}
+CounterExampleVisualizer.$inject = ["animation", "eventBus", "elementRegistry"];
