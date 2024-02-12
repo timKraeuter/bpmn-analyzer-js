eb35e0741e42330d370002c21fff0db436118f7b
Tim Kräuter <tim.kraeuterr@gmail.com>
2024-02-12 14:08:05 +0100

    Add click listener for the animation

--- src/counter-example-visualization/CounterExampleVisualizer.js
+++ src/counter-example-visualization/CounterExampleVisualizer.js
@@ -2,9 +2,34 @@ export default function CounterExampleVisualizer(animation, eventBus) {
   this._animation = animation;
   this._eventBus = eventBus;
 
-  this._eventBus.on("counterexample", (event) => {
-    console.log("counterexample", event);
+  this._eventBus.on("analysis.done", (result) => {
+    result.property_results.forEach((propertyResult) => {
+      addClickListenerIfNotFulfilled(propertyResult);
+    });
   });
 }
 
 CounterExampleVisualizer.$inject = ["animation", "eventBus"];
+
+function addClickListenerIfNotFulfilled(propertyResult) {
+  const property = document.getElementById(propertyResult.property);
+  if (!property.fulfilled && propertyResult.counter_example) {
+    property.classList.add("clickable");
+    property.addEventListener("click", function () {
+      visualizeCounterExample(propertyResult);
+    });
+  } else {
+    property.classList.remove("clickable");
+  }
+}
+
+/**
+ * Visualize the counter example on click.
+ * @param {PropertyResult} propertyResult
+ */
+function visualizeCounterExample(propertyResult) {
+  console.log("Start animation", propertyResult.counter_example.start_state);
+  propertyResult.counter_example.transitions.forEach((transition) => {
+    console.log("Animate each transition", transition);
+  });
+}
