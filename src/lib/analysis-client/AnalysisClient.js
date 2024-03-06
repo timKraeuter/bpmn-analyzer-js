343a95a2ca45965e649371419158c3e1dc952d92
Tim Kräuter <tim.kraeuterr@gmail.com>
2024-03-06 11:16:18 +0100

    Port stuff

--- src/lib/analysis-client/AnalysisClient.js
+++ src/lib/analysis-client/AnalysisClient.js
@@ -12,28 +12,34 @@ function getCheckerPort() {
   const port = window.location.port;
   // Dev is served at + 1.
   if (port === "3000") {
-    return parseInt(port) + 1;
+    return ":" + (parseInt(port) + 1);
+  }
+  if (port === "") {
+    return "";
   }
   // We assume the checker function is served at the same port.
-  return parseInt(port);
+  return ":" + port;
 }
 
 function startAnalysis(checker_port, diagramXML, eventBus) {
-  fetch(`http://localhost:${checker_port}/check_bpmn`, {
-    method: "POST",
-    body: JSON.stringify({
-      bpmn_file_content: diagramXML.xml,
-      properties_to_be_checked: [
-        "Safeness",
-        "OptionToComplete",
-        "ProperCompletion",
-        "NoDeadActivities",
-      ],
-    }),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8",
+  fetch(
+    `${window.location.protocol}//${window.location.hostname}${checker_port}/check_bpmn`,
+    {
+      method: "POST",
+      body: JSON.stringify({
+        bpmn_file_content: diagramXML.xml,
+        properties_to_be_checked: [
+          "Safeness",
+          "OptionToComplete",
+          "ProperCompletion",
+          "NoDeadActivities",
+        ],
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
     },
-  })
+  )
     .then((response) => response.json())
     .then((json) => {
       eventBus.fire("analysis.done", json);
