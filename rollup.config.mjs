7801a8a1b5d49077058ade66ed035b3d118b19cd
Tim Kräuter <tim.kraeuterr@gmail.com>
2024-04-29 23:45:01 +0200

    Rollup just missing css from bpmn-js now

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,26 +3,19 @@
 import terser from "@rollup/plugin-terser";
 import nodeResolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import json from "@rollup/plugin-json";
 import replace from "@rollup/plugin-replace";
+import { string } from "rollup-plugin-string";
+import copy from 'rollup-plugin-copy'
 
-const outputDir = "dist";
+const outputDir = "public";
+// TODO: Add copying bpmn-js css
 
 const configs = [
   {
     input: `./src/app.js`,
     output: {
-      name: "BpmnJS",
-      file: `${outputDir}/bpmn-analyzer-js.development.js`,
-      format: "umd",
-    },
-    plugins: pgl([], "development"),
-  },
-  {
-    input: `./src/app.js`,
-    output: {
-      name: "BpmnJS",
-      file: `${outputDir}/bpmn-analyzer-js.production.min.js`,
+      name: "bpmn-analyzer-js",
+      file: `${outputDir}/app.js`,  // new config for production and -c configName in npm
       format: "umd",
     },
     plugins: pgl(
@@ -48,9 +41,18 @@ function pgl(plugins = [], env = "production") {
       preventAssignment: true,
       "process.env.NODE_ENV": JSON.stringify(env),
     }),
+    string({
+      include: "resources/*.bpmn"
+    }),
+    copy({
+      targets: [
+        { src: 'src/index.html', dest: `${outputDir}/` },
+        { src: 'src/favicon.ico', dest: `${outputDir}/` },
+        { src: 'src/css/**', dest: `${outputDir}/css` },
+      ]
+    }),
     nodeResolve(),
     commonjs(),
-    json(),
     ...plugins,
   ];
 }
