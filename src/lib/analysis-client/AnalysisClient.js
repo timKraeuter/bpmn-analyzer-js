3e22f425b8c6324f52161ef49827c5bffdac6f0e
Tim Kräuter <tim.kraeuterr@gmail.com>
2024-04-29 15:32:15 +0200

    First wasm test

--- src/lib/analysis-client/AnalysisClient.js
+++ src/lib/analysis-client/AnalysisClient.js
@@ -1,50 +1,17 @@
-export default function AnalysisClient(eventBus) {
-  let checker_port = getCheckerPort();
+import init, { check_bpmn } from "../../pkg";
 
+export default function AnalysisClient(eventBus) {
   eventBus.on("analysis.start", (diagramXML) => {
-    startAnalysis(checker_port, diagramXML, eventBus);
+    startAnalysis(diagramXML, eventBus);
   });
 }
 
 AnalysisClient.$inject = ["eventBus"];
 
-function getCheckerPort() {
-  const port = window.location.port;
-  // Dev is served at + 1.
-  if (port === "3000") {
-    return ":" + (parseInt(port) + 1);
-  }
-  if (port === "") {
-    return "";
-  }
-  // We assume the checker function is served at the same port.
-  return ":" + port;
-}
-
-function startAnalysis(checker_port, diagramXML, eventBus) {
-  fetch(
-    `${window.location.protocol}//${window.location.hostname}${checker_port}/check_bpmn`,
-    {
-      method: "POST",
-      body: JSON.stringify({
-        bpmn_file_content: diagramXML.xml,
-        properties_to_be_checked: [
-          "Safeness",
-          "OptionToComplete",
-          "ProperCompletion",
-          "NoDeadActivities",
-        ],
-      }),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    },
-  )
-    .then((response) => response.json())
-    .then((json) => {
-      eventBus.fire("analysis.done", json);
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
+function startAnalysis(diagramXML, eventBus) {
+  init().then(() => {
+    check_bpmn(diagramXML.xml);
+    // TODO: Get return and check performance
+    // eventBus.fire("analysis.done", json);
+  });
 }
