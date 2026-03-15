2ae2f91ca56bb9f717b72e815138da7e96fdc3fa
Tim Kräuter <tim.kraeuterr@gmail.com>
2026-03-15 20:12:14 +0100

    Split vendor chunks to fix large bundle warning
    Use Rolldown codeSplitting to separate node_modules into dedicated
    chunks: diagram-js (254 kB), bpmn-js/moddle (300 kB), other vendors
    (5 kB), and app code (157 kB). All chunks are under the 500 kB limit.

--- vite.config.js
+++ vite.config.js
@@ -32,13 +32,31 @@ export default defineConfig(({ mode }) => {
       outDir,
       emptyOutDir: true,
       sourcemap: !isProduction,
-      chunkSizeWarningLimit: 750,
       rolldownOptions: {
         input: resolve(__dirname, "index.html"),
         output: {
           entryFileNames: "app.js",
           chunkFileNames: "[name].js",
           assetFileNames: "[name][extname]",
+          codeSplitting: {
+            groups: [
+              {
+                name: "diagram-vendor",
+                test: /node_modules[\\/]diagram-js/,
+                priority: 20,
+              },
+              {
+                name: "bpmn-vendor",
+                test: /node_modules[\\/](bpmn-js|bpmn-moddle|moddle)/,
+                priority: 15,
+              },
+              {
+                name: "vendor",
+                test: /node_modules/,
+                priority: 10,
+              },
+            ],
+          },
         },
       },
     },
